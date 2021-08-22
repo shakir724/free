a197931682cf3654d6970bf29e30a4a829ae9326
shakir724 <shakir922001@gmail.com>
2021-08-22 14:44:07 +0530

    fornt end ready

---
+++ sidebar.php
@@ -0,0 +1,112 @@
+<div class="col-lg-4">
+				<!-- JOIN US STARTS -->
+				<div class="join-us">
+					<div class="row category-caption">
+						<div class="col-lg-12">
+							<h2 class="pull-left">FOLLOW US</h2>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-12">
+							<ul>
+								<li class="facebook">
+									<div class="icon"><a href="#"><i class="fa fa-facebook"></i></a></div>
+								</li>
+								<li class="twitter">
+									<div class="icon"><a href="#"><i class="fa fa-twitter"></i></a></div>
+								</li>
+								<li class="linkedin">
+									<div class="icon"><a href="#"><i class="fa fa-linkedin"></i></a></div>
+								</li>
+								<li class="dribbble">
+									<div class="icon"><a href="#"><i class="fa fa-dribbble"></i></a></div>
+								</li>
+								<li class="facebook">
+									<div class="icon"><a href="#"><i class="fa fa-facebook"></i></a></div>
+								</li>
+								<li class="twitter">
+									<div class="icon"><a href="#"><i class="fa fa-twitter"></i></a></div>
+								</li>
+								<li class="linkedin">
+									<div class="icon"><a href="#"><i class="fa fa-linkedin"></i></a></div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- JOIN US ENDS -->
+				<!-- TABS STARTS -->
+				<div class="tabs">
+					<div class="pull-left">
+						<!-- Nav tabs -->
+						<h4 >Tags</h4>
+						<!-- Tab panes -->
+						<div class="tab-content">
+							
+							<!-- TAG STARTS -->
+							<div >
+								<div class="tag-list">
+									<a href="#">design</a>
+									<a href="#">graphics</a>
+									<a href="#">photography</a>
+									<a href="#">html5</a>
+									<a href="#">wordpress</a>
+									<a href="#">onepage</a>
+									<a href="#">design</a>
+									<a href="#">graphics</a>
+									<a href="#">photography</a>
+									<a href="#">html5</a>
+									<a href="#">wordpress</a>
+									<a href="#">onepage</a>
+								</div>
+							</div>
+							<!-- TAGS STARTS -->
+						</div>
+					</div>
+				</div>
+				<!-- TABS ENDS -->
+				<!-- ADVERTISEMENT 300 X 250 STARTS -->
+				<div class="ad-300x250">
+					<img src="images/ads/3d_300x250_v2.gif" width="300" height="250" alt="">
+				</div>
+				<!-- ADVERTISEMENT 300 X 250 ENDS -->
+			
+				<!-- FEATURED AUTHORS ENDS -->
+				<!-- JOIN NEWSLETTER STARTS -->
+				<div class="newsletter-block">
+					<div class="row category-caption">
+						<div class="col-lg-12">
+							<h2 class="pull-left">JOIN OUR NEWSLETTER</h2>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-12"><input name="" type="text" placeholder="Email Address"></div>
+						<div class="col-lg-6"><input name="" type="text" placeholder="First Name"></div>
+						<div class="col-lg-6"><input name="" type="text" placeholder="Last Name"></div>
+						<div class="col-lg-12"><input type="button" value="SUBSCRIBE"></div>
+					</div>
+				</div>
+				<!-- JOIN NEWSLETTER ENDS -->
+				<!-- ADVERTISEMENT 125 x 125 STARTS -->
+				<div class="ad-125x125">
+					<div class="row">
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><img src="images/ads/125x125/1.jpg" width="125"
+								height="125" alt=""></div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><img src="images/ads/125x125/2.jpg" width="125"
+								height="125" alt=""></div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><img src="images/ads/125x125/3.jpg" width="125"
+								height="125" alt=""></div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><img src="images/ads/125x125/5.gif" width="125"
+								height="125" alt=""></div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><img src="images/ads/125x125/4.jpg" width="125"
+								height="125" alt=""></div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><img src="images/ads/125x125/6.gif" width="125"
+								height="125" alt=""></div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><img src="images/ads/125x125/2.jpg" width="125"
+								height="125" alt=""></div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><img src="images/ads/125x125/3.jpg" width="125"
+								height="125" alt=""></div>
+					</div>
+				</div>
+				<!-- ADVERTISEMENT 300 X 250 ENDS -->
+			</div>
